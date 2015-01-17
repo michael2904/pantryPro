e8e74d2dd39e18db60a7a1a7be23a8a6e99d3b50
michael2904 <michael.abdallah.minciotti@gmail.com>
2015-01-17 09:54:15 -0500

    Initial Commit

---
+++ myFridge2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  myFridge2
+//
+//  Created by Michael Abdallah-Minciotti on 2015-01-17.
+//  Copyright (c) 2015 Michael Abdallah-Minciotti. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
